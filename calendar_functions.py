edee6f587f1d5076e493f70dfb46d7b6442e3a4e
AxelPribadi <64358814+AxelPribadi@users.noreply.github.com>
2025-04-11 17:16:26 +0800

    working calendar MCP

--- calendar_functions.py
+++ calendar_functions.py
@@ -1,14 +1,21 @@
-import datetime
 import os.path
+import re
+
+import datetime
+from datetime import datetime
+from dateutil import parser, tz
+from typing import List, Optional, Any
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 def check_auth():
+    """Authenticate Google Calendar API"""
     creds=None
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
@@ -59,8 +66,69 @@ def get_events(
     except Exception as e:
         print(f"Error fetching calendar events: {e}")
         return []
+
+def add_events(event):
+    creds = check_auth()
     
+    try:
+        service = build("calendar", "v3", credentials=creds)
+        
+        # update calendar with event
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        
+        print("Event Added")
+        return event.get('htmlLink')
+
+    except Exception as e:
+        print(f"Error in creating the event: {e}")
+
+def standardize_datetime(date_str: str):
+    # Check if string is already in ISO 8601 format
+    iso_pattern = re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d+)?(Z|[+-]\d{2}:\d{2})$')
+    if iso_pattern.match(date_str):
+        return date_str
+    
+    else:
+        parsed_date = parser.parse(date_str).replace(microsecond=0)
+
+        # Check if timezone is already included
+        if parsed_date.tzinfo is not None:
+            return parsed_date.isoformat()
+
+        # Assume date provided is in localtime
+        else:
+            local_tz = tz.tzlocal()
+            date_local = parsed_date.replace(tzinfo=local_tz)    
+            return date_local.isoformat()
+
 if __name__ == "__main__":
-    events = get_events(10)
+    events = get_events(
+        # time_start="2025-04-10T00:00:00+08:00",
+        # time_end="2025-04-13T00:00:00+08:00",
+        top_n=10
+    )
+
     for event in events:
-        print(event)
+        if event["eventType"] == "default":
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
+
+
+    # now = datetime.now().replace(microsecond=0)
+    # start = (now + timedelta(hours=4)).isoformat()
+    # end = (now + timedelta(hours=6)).isoformat()
+    # timezone = get_localzone_name()
+
+    # event = create_event(
+    #     summary="Kwargs Test",
+    #     start_time=start,
+    #     end_time=end,
+    #     timezone=timezone,
+    #     location="Online",
+    #     description="This event uses the Calendar API",
+    #     attendees=[{"email": "glorianath20@gmail.com"}]
+    # )
+
+    # html_link = add_events(event)
+    # print(html_link)
+
--- calendar_server.py
+++ calendar_server.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from dateutil.parser import isoparse
+from dateutil import parser
+from tzlocal import get_localzone_name
 from mcp.server.fastmcp import FastMCP
 
 from calendar_functions import * 
@@ -7,15 +8,21 @@ from calendar_functions import *
 mcp = FastMCP("Calendar Helper")
 
 @mcp.tool()
-async def read_calendar(
-    time_start: str,
-    time_end: str,
+def read_calendar(
+    start_time: str,
+    end_time: str,
     top_n: int
 ) -> dict:
-    """Read Google Calendar events from a selected timeframe (in ISO Date Format)"""
-    print(f"Fetching events between {time_start} and {time_end}")
+    """Fetch `top_n` Google Calendar events between a timeframe (in ISO format)"""
+
+    # convert to iso format
+    start_time = standardize_datetime(start_time)
+    end_time = standardize_datetime(end_time)
+   
+    print(f"Fetching events between {start_time} and {end_time}")
+    
     try:
-        events = get_events(top_n, time_start, time_end)
+        events = get_events(top_n, start_time, end_time)
         print(f"Events found: {len(events)}")
         
         if not events:
@@ -28,6 +35,69 @@ async def read_calendar(
         print(f"Error in read_calendar: {e}")
         return {"error": str(e)}
 
+@mcp.tool()
+def create_events(
+    summary: str,
+    start_time: Optional[str],
+    end_time: Optional[str],
+    location: str=None,
+    description: Optional[str]=None,
+    attendees: Optional[List[dict]]=None
+):
+    """Create a new Google Calendar event with the provided details"""
+
+    # convert to iso format
+    start_time = standardize_datetime(start_time)
+    end_time = standardize_datetime(end_time)
+    timezone = get_localzone_name()
+    
+    event = {
+        "summary": summary,
+        "location": location,
+        "description": description,
+        "start": {
+            "dateTime": start_time,
+            "timeZone": timezone,
+        },
+        "end": {
+            "dateTime": end_time,
+            "timeZone": timezone,
+        },
+    }
+
+    if attendees:
+        event["attendees"] = attendees
+
+    try:
+        link = add_events(event)
+
+        print("Successfully added event to calendar")
+        return {"link": link}
+    
+    except Exception as e:
+        print(f"Error in inserting event: {e}")
+        return {"error": str(e)}
+
+
 if __name__ == "__main__":
     # Initialize and run the server
     mcp.run(transport='stdio')
+
+    # events = read_calendar(
+    #     start_time="4/11/2025",
+    #     end_time="16/4/2025",
+    #     top_n=10
+    # )
+    # print(events)
+    
+
+    # event = create_events(
+    #     summary="Attendee Test",
+    #     start_time="2025-04-13 21:00",
+    #     end_time="2025-04-13 22:00",
+    #     location="Online",
+    #     description="Axel's Test. Hopefully works with Claude",
+    #     attendees=[{"email": "glorianath20@gmail.com"}]
+    # )
+    # print(event)
+    
